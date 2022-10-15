a726dae901188863e3a61b164ed95a608228fc61
kenji <hotdrop.tkenji@gmail.com>
2022-10-15 11:02:44 +0900

    generateで出ていた警告を解消

--- lib/repository/remote/models/point_response.dart
+++ lib/repository/remote/models/point_response.dart
@@ -4,7 +4,7 @@ part 'point_response.freezed.dart';
 part 'point_response.g.dart';
 
 @freezed
-abstract class PointResponse with _$PointResponse {
+class PointResponse with _$PointResponse {
   factory PointResponse(int point) = _PointResponse;
 
   factory PointResponse.fromJson(Map<String, Object?> json) => _$PointResponseFromJson(json);
--- lib/repository/remote/models/user_response.dart
+++ lib/repository/remote/models/user_response.dart
@@ -4,7 +4,7 @@ part 'user_response.freezed.dart';
 part 'user_response.g.dart';
 
 @freezed
-abstract class UserResponse with _$UserResponse {
+class UserResponse with _$UserResponse {
   factory UserResponse(String userId) = _UserResponse;
 
   factory UserResponse.fromJson(Map<String, Object?> json) => _$UserResponseFromJson(json);
